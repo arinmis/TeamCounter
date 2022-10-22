316df1c1ebf7430f0bbd8f97556809d97bb1d9c6
arinmis <mustafa.arinmis@linarit.com>
2022-10-22 18:08:30 +0300

    Create hello world app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>hello Team Counter</Text>
+    </View>
+  );
+};
+
+export default App;
