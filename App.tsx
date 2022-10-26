dfff4a0ce26573a15798072e715f4ea788fa598b
arinmis <mustafa_arinmis@outlook.com>
2022-10-26 22:36:03 +0300

    Setup React navigatior

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+// In App.js in a new project
 
-const App = () => {
+import * as React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
-    <View>
-      <Text>hello Team Counter</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
