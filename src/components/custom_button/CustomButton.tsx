ec639b56194ed2b8dc163fa38ca3bcd25d1667f3
arinmis <mustafa.arinmis@linarit.com>
2022-10-27 01:06:52 +0300

    Update custom button style

--- src/components/custom_button/CustomButton.tsx
+++ src/components/custom_button/CustomButton.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
-import {Button} from 'react-native';
+import {StyleSheet, TouchableOpacity, Text} from 'react-native';
 
-const CustomButton = ({onPress, title}) => {
-  return <Button onPress={onPress} title={title} />;
-};
+function CustomButton({onPress, title}) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    alignSelf: 'flex-start',
+    borderRadius: 10,
+    backgroundColor: '#008080',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+  },
+  text: {
+    color: 'white',
+  },
+});
 
 export default CustomButton;
